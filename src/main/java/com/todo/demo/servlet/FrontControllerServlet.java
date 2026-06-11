ec819c14614c1e16e6de0dcd27fac2d90aba9f5c
brandon0638 <brandonandria06@gmail.com>
2026-06-11 19:39:10 +0200

    creation methode doGet

---
+++ src/main/java/com/todo/demo/servlet/FrontControllerServlet.java
@@ -0,0 +1,17 @@
+package com.todo.demo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "FrontControllerServlet", urlPatterns = "/*")
+public class FrontControllerServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    }
+}
