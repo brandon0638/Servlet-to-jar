b081f5fe96413f001108dfeccf41ecbfedf368fe
brandon0638 <brandonandria06@gmail.com>
2026-06-11 19:40:12 +0200

    appel methode processRequest dans doGet

--- src/main/java/com/todo/demo/servlet/FrontControllerServlet.java
+++ src/main/java/com/todo/demo/servlet/FrontControllerServlet.java
@@ -12,6 +12,6 @@ public class FrontControllerServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-
+        processRequest(request,response);
     }
 }
