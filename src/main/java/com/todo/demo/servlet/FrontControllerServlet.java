abccbc587f70c869d7afc127ac18ecd5ff40522d
brandon0638 <brandonandria06@gmail.com>
2026-06-12 10:01:23 +0200

    copie de jar dans lib

--- src/main/java/com/todo/demo/servlet/FrontControllerServlet.java
+++ src/main/java/com/todo/demo/servlet/FrontControllerServlet.java
@@ -5,11 +5,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.util.Map;
 
-@WebServlet(name = "FrontControllerServlet", urlPatterns = "/*")
+// @WebServlet(name = "FrontControllerServlet", urlPatterns = "/*")
 public class FrontControllerServlet extends HttpServlet{
 
     
