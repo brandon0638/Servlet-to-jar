20d7a81a2364fb0a5cef932bdb6ac43cfc82e0c7
brandon0638 <brandonandria06@gmail.com>
2026-06-18 20:26:18 +0200

    creation annotation

---
+++ projet_sprint1/src/main/java/com/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.controller;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME) //l'annotation existe pendant execution
+@Target(ElementType.TYPE) //TYPE = classe , METHOD = methode, FIELD = variable
+public @interface Controller{
+
+}
