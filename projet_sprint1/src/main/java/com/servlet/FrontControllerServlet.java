061af47f53a03a45b2e3cb2da7f77b144081195b
brandon0638 <brandonandria06@gmail.com>
2026-06-18 22:02:32 +0200

    mis a jour de frontservlet

--- projet_sprint1/src/main/java/com/servlet/FrontControllerServlet.java
+++ projet_sprint1/src/main/java/com/servlet/FrontControllerServlet.java
@@ -1,35 +1,65 @@
 package com.servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.util.AnnotationUtil;
+import com.annotation.Controllerako;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.List;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 public class FrontControllerServlet extends HttpServlet{
 
+    private List<Class<?>> controllers;
+
+   @Override
+    public void init() {
+
+        String basePackage = "com.test.controller";
+
+        List<Class<?>> classes = AnnotationUtil.getClasses(basePackage);
+
+        controllers = AnnotationUtil.getControllers(basePackage);
+
+        System.out.println("Framework initialisé");
+        System.out.println("Controllers trouvés: " + controllers.size());
+    }
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         processRequest(request,response);
+        
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         processRequest(request,response);
+        
     }
 
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         
-        String url = request.getRequestURL().toString();
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+
         out.println("<h1>URL actuelle</h1>");
-        out.println("<p>" + url + "</p>");
+        out.println("<p>" + request.getRequestURL() + "</p>");
+
+        out.println("<h2>Controllers détectés</h2>");
+        out.println("<ul>");
+
+        for (Class<?> c : controllers) {
+            out.println("<li>" + c.getName() + "</li>");
+        }
+
+        out.println("</ul>");
     }
 
     
+
+
 }
---
+++ projet_sprint1/src/main/java/com/util/AnnotationUtil.java
@@ -0,0 +1,62 @@
+package com.util;
+
+import com.annotation.Controllerako;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnotationUtil {
+
+    public static List<Class<?>> getControllers(String basePackage) {
+
+        List<Class<?>> result = new ArrayList<>();
+
+        List<Class<?>> classes = getClasses(basePackage);
+
+        for (Class<?> c : classes) {
+
+            if (c.isAnnotationPresent(Controllerako.class)) {
+                result.add(c);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Method> getAnnotatedMethods(Class<?> c,
+            Class annotation) {
+
+        List<Method> result = new ArrayList<>();
+
+        for (Method m : c.getDeclaredMethods()) {
+
+            if (m.isAnnotationPresent(annotation)) {
+                result.add(m);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Field> getAnnotatedFields(Class<?> c,
+            Class annotation) {
+
+        List<Field> result = new ArrayList<>();
+
+        for (Field f : c.getDeclaredFields()) {
+
+            if (f.isAnnotationPresent(annotation)) {
+                result.add(f);
+            }
+        }
+
+        return result;
+    }
+
+   
+    public static List<Class<?>> getClasses(String basePackage) {
+        return new ArrayList<>();
+    }
+}
