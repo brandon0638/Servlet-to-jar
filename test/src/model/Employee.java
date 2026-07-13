86090226967c01eb792d8e38ab2deac63f3846f6
brandon0638 <brandonandria06@gmail.com>
2026-07-13 19:56:04 +0200

    model

---
+++ test/src/model/Employee.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Employee {
+
+    private int id;
+    private String nom;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
